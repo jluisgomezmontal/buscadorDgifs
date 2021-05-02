9f5f8ea60cf153454f6601e84d0d8e7f7706782a
Luis <montalgex18@gmail.com>
2021-05-02 13:54:11 -0500

    f

---
+++ docs/precache-manifest.f6095d7d662a4a47cef229caecf95a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2b4cec2fd7d99d1aa0d8fdfaef6fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb66b30a704a855eceae",
+    "url": "/static/css/main.e0ab498c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb66b30a704a855eceae",
+    "url": "/static/js/main.97aa3c02.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
